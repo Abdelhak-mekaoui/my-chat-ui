97220ec2c8779ac43ea87f181520e671b4d3941f
Abdelhak MEKAOUI <mekaouiabdelhak1@gmail.com>
2024-04-09 17:55:18 +0000

    responsive sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,8 @@
 
 'use client'
-import React from "react";
+import React, { useState } from "react";
+import { IoIosArrowDropright } from "react-icons/io";
+
 import {
     Card,
     Typography,
@@ -13,15 +15,35 @@ import {
   import { IoSettingsOutline } from "react-icons/io5";
 import Conversations from "./Conversations";
 import { useSession } from "next-auth/react";
+import { BsLayoutSidebarInset } from "react-icons/bs";
+
 import { useRouter } from "next/navigation";
   const Sidebar: React.FC = () => {
     const { data: session } = useSession(); 
     const router = useRouter();
+    const [isSidebarOpen, setSidebarOpen] = useState<Boolean>(false);
+
     if (!session) return <Card placeholder="d" onPointerEnterCapture={()=>console.log('pointer')} onPointerLeaveCapture={()=>console.log('pointer')} className="bg-base-200 mt-2 w-full max-w-[15rem] md:max-w-[16rem] lg:max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 "><span className="mx-auto my-auto">Please log in to use the app</span>
   </Card>;
 
     return (
-      <Card placeholder="d" onPointerEnterCapture={()=>console.log('pointer')} onPointerLeaveCapture={()=>console.log('pointer')} className="bg-base-200 mt-2 w-full max-w-[15rem] md:max-w-[16rem] lg:max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 ">
+      <>
+      <button 
+  className="md:hidden" 
+  onClick={() => setSidebarOpen(!isSidebarOpen)}
+>
+<BsLayoutSidebarInset className="h-6 w-6" />
+
+</button>
+{isSidebarOpen && (
+  <div 
+    className="fixed inset-0 bg-black opacity-50 z-20 md:hidden" 
+    onClick={() => setSidebarOpen(false)}
+  ></div>
+)}
+      <Card placeholder="d" onPointerEnterCapture={()=>console.log('pointer')} onPointerLeaveCapture={()=>console.log('pointer')} className={`left-0 my-2 w-70 bg-base-200 p-4 fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30 ${
+  isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+} md:translate-x-0 md:w-full md:max-w-80 md:p-4 md:static md:h-auto`}>
        
         <List placeholder="d" onPointerEnterCapture={()=>console.log('pointer')} onPointerLeaveCapture={()=>console.log('pointer')}>
           <ListItem placeholder="d" onPointerEnterCapture={()=>console.log('pointer')} onPointerLeaveCapture={()=>console.log('pointer')} className="text-base-content">
@@ -50,7 +72,7 @@ import { useRouter } from "next/navigation";
           </ListItem>
 
         </List>
-      </Card>
+      </Card></>
     );
   }
 
